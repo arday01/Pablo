eb3f821b0f37b2dae8f2b9c49879998d407caa32
Kürşat Kilitci <121718589+SalierK@users.noreply.github.com>
2023-04-28 20:21:31 +0300

    Olum Bolgesi Hata Düzeltmesi
    Karakter Hasar aldıktan sonraki 3 saniye boyunca hasar görmez oluyordu eğer o sırada ölürse oyunda yeniden doğduğu zaman ölümsüz olarak devam ediyordu bu hatayı fixledik

--- Assets/Scripts/Player/PlayerCollision.cs
+++ Assets/Scripts/Player/PlayerCollision.cs
@@ -3,34 +3,48 @@ using UnityEngine;
 using System.Collections;
 public class PlayerCollision : MonoBehaviour
 {
+    private PlayerMovement playerMovement;
+
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.transform.tag == "Enemy")
+        if (collision.transform.tag == "Enemy")
         {
-            Debug.Log("Olan");
+  
             HealthManager.health--;
-            if(HealthManager.health <=0){
+            if (HealthManager.health <= 0)
+            {
                 PlayerManager.isGameOver = true;
                 AudioManager.instance.Play("GameOver");
                 gameObject.SetActive(false);
-            }else{
+            }
+            else
+            {
                 StartCoroutine(GetHurt());
             }
         }
+
         if (collision.transform.tag == "DeadZone")
         {
-            Debug.Log("Ýstenen");
-            PlayerManager.isGameOver = true;
-            AudioManager.instance.Play("GameOver");
-            gameObject.SetActive(false);
+
+            HealthManager.health -= 3;
+            if (HealthManager.health <= 0)
+            {
+                Physics2D.IgnoreLayerCollision(6, 8, false);
+                PlayerManager.isGameOver = true;
+                AudioManager.instance.Play("GameOver");
+                gameObject.SetActive(false);
+            }
         }
     }
 
-    IEnumerator GetHurt(){ 
-        Physics2D.IgnoreLayerCollision(6,8);
+    IEnumerator GetHurt()
+    {
+        Physics2D.IgnoreLayerCollision(6, 8);
         GetComponent<Animator>().SetLayerWeight(1, 1);
         yield return new WaitForSeconds(3);
         GetComponent<Animator>().SetLayerWeight(1, 0);
-        Physics2D.IgnoreLayerCollision(6,8, false);
+        Physics2D.IgnoreLayerCollision(6, 8, false);
     }
+
 }
